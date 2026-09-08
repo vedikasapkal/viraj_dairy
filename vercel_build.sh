90a687df636c128b9768464af09ff9e05e2d08cc
Vedika Sapkal <vedikasapkal196@gmail.com>
2026-09-08 11:09:31 +0530

    Add executable vercel_build script

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# Clone Flutter SDK (stable channel)
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
-
-# Check Flutter version & enable web
-flutter doctor
 flutter config --enable-web
-
-# Install packages and build web bundle
 flutter pub get
 flutter build web --release
